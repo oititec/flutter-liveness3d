bb54a42d8dd6fa31b97e2897839c958fbb89a21c
Vitor Souza <vitorx1280@gmail.com>
2023-07-19 15:04:03 -0300

    style: removing unused imports

--- test/oiti_liveness3d_test.dart
+++ test/oiti_liveness3d_test.dart
@@ -1,5 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:oiti_liveness3d/oiti_liveness3d_method_channel.dart';
 import 'package:oiti_liveness3d/oiti_liveness3d_platform_interface.dart';
 
 void main() {
